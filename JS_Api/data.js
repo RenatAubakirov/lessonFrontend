4c479d2a819235a742ade385ea34dc85dcfd51a8
RenatAubakirov <arenat98@mail.ru>
2025-02-20 17:10:09 +0700

    dz1

---
+++ JS_Api/data.js
@@ -0,0 +1,22 @@
+const lessonsData = `
+[
+  {
+    "name": "Math",
+    "time": "18:30",
+    "maxMembers": 5,
+    "currentMembers": 0
+  },
+  {
+    "name": "Physics",
+    "time": "15:30",
+    "maxMembers": 7,
+    "currentMembers": 1
+  },
+  {
+    "name": "JavaScript",
+    "time": "13:30",
+    "maxMembers": 6,
+    "currentMembers": 3
+  }
+]
+`;
